4bf5320592c374285088a725a882ad876fe4fc51
Oliinyk <vladyslavx.oliinyk@intel.com>
2018-06-05 19:08:44 +0300

    Add new features
    Signed-off-by: Oliinyk <vladyslavx.oliinyk@intel.com>

---
+++ newFile.py
@@ -0,0 +1,11 @@
+ Volume in drive C is UFD-Windows
+ Volume Serial Number is D606-D328
+
+ Directory of C:\Program Files\Git\TestDir
+
+06/05/2018  07:05 PM    <DIR>          .
+06/05/2018  07:05 PM    <DIR>          ..
+06/05/2018  07:01 PM                 5 bla.c
+06/05/2018  07:05 PM                 0 newFile.py
+               2 File(s)              5 bytes
+               2 Dir(s)  31,894,822,912 bytes free
